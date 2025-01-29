fbc0b8641c9fa9a97a9c1e4a04d78d6d75ded550
Mark <19152780+WavyOneKenobi3@users.noreply.github.com>
2025-01-29 10:58:54 -0500

    added gamedata file

---
+++ game.data.py
@@ -0,0 +1,103 @@
+teams = [
+{	
+ "team": "Dallas Cowboys",
+ "worth": 9.0,
+ "Sport": "NFL Football", 
+ "city/contry": "Dallas,TX USA"
+},
+{	
+ "team": "New York Yankees",
+ "worth": 7.1,
+ "Sport": "MLB Baseball", 
+ "city/contry": "New York,NY USA"
+},
+{	
+ "team": "Golden State Warriors",
+ "worth": 7.0,
+ "Sport": "NBA Basketball", 
+ "city/contry": "San Francisco,CA USA"
+},
+{	
+ "team": "New England",
+ "worth": 7.0,
+ "Sport": "NFL Football:", 
+ "city/contry": "Foxborough,MA USA"
+},
+{	
+ "team": "Los Angeles",
+ "worth": 6.9,
+ "Sport": "NFL Football", 
+ "city/contry": "Los Angeles,CA USA"
+},
+{	
+ "team": "New York Giants",
+ "worth": 6.8,
+ "Sport": "NFL Football", 
+ "city/contry": "New York,NY USA"
+},
+{	
+ "team": "Chicago Bears",
+ "worth": 6.3,
+ "Sport": "NFL Football", 
+ "city/contry": "Chicago,IL  USA"
+},
+{	
+ "team": "Las Vegas Raiders",
+ "worth": 6.2,
+ "Sport": "NFL Football", 
+ "city/contry": "Las Vegas,NV  USA"
+},
+{	
+ "team": "New York Knick",
+ "worth": 6.1,
+ "Sport": "NBA Basketball", 
+ "city/contry": "New York,NY  USA"
+},
+{	
+ "team": "New York Jets",
+ "worth": 6.1,
+ "Sport": "NFL Football", 
+ "city/contry": "New York,NY  USA"
+},
+{	
+ "team": "Real Madrid",
+ "worth": 6.1,
+ "Sport": "Soccar", 
+ "city/contry": "Madrid, Spain"
+},
+{	
+ "team": "Washington Commanders",
+ "worth": 6.1,
+ "Sport": "NFL Football", 
+ "city/contry": "Washington,DC  USA"
+},
+{	
+ "team": "Manchester United",
+ "worth": 6.0,
+ "Sport": "Soccar", 
+ "city/contry": "Old Trafford, Greater Manchester, England"
+},
+{	
+ "team": "San Francisco 49ers",
+ "worth": 6.0,
+ "Sport": "NFL Football", 
+ "city/contry": "San Francisco,CA USA"
+},
+{	
+ "team": "Los Angeles Lakers",
+ "worth": 5.9,
+ "Sport": "NBA Basketball", 
+ "city/contry": "Los angeles,CA USA"
+}
+]
+
+'''
+- which sports team is worth?
+	- list of teams and worth [dictionary w/ key and values]
+ 
+		- comparison line: team, worth, sport, city/country
+	- comparing the teams
+		- make A vs B, once get it right change B to A then B is a different team
+	- letting the user choose between
+	- function deciding right and wrong
+	- add a counter on how many you got right, then once lost end game'''
--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-'''
-- which sports team is worth?
-	- list of teams and worth [dictionary w/ key and values]
-		- comparison line: team, worth, sport, city/country
-	- comparing the teams
-		- make A vs B, once get it right change B to A then B is a different team
-	- letting the user choose between
-	- function deciding right and wrong
-	- add a counter on how many you got right, then once lost end game'''
+import game.data
+
