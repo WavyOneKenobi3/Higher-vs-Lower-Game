dab16c9ec2bf3a11db823a30e0822ae61c2c4a75
Mark <19152780+WavyOneKenobi3@users.noreply.github.com>
2025-01-29 14:19:41 -0500

    update

--- gamedata.py
+++ gamedata.py
@@ -3,101 +3,91 @@ teams = [
  "team": "Dallas Cowboys",
  "worth": 9.0,
  "Sport": "NFL Football", 
- "city/contry": "Dallas,TX USA"
+ "city/country": "Dallas,TX USA"
 },
 {	
  "team": "New York Yankees",
  "worth": 7.1,
  "Sport": "MLB Baseball", 
- "city/contry": "New York,NY USA"
+ "city/country": "New York,NY USA"
 },
 {	
  "team": "Golden State Warriors",
  "worth": 7.0,
  "Sport": "NBA Basketball", 
- "city/contry": "San Francisco,CA USA"
+ "city/country": "San Francisco,CA USA"
 },
 {	
  "team": "New England",
  "worth": 7.0,
  "Sport": "NFL Football:", 
- "city/contry": "Foxborough,MA USA"
+ "city/country": "Foxborough,MA USA"
 },
 {	
  "team": "Los Angeles",
  "worth": 6.9,
  "Sport": "NFL Football", 
- "city/contry": "Los Angeles,CA USA"
+ "city/country": "Los Angeles,CA USA"
 },
 {	
  "team": "New York Giants",
  "worth": 6.8,
  "Sport": "NFL Football", 
- "city/contry": "New York,NY USA"
+ "city/country": "New York,NY USA"
 },
 {	
  "team": "Chicago Bears",
  "worth": 6.3,
  "Sport": "NFL Football", 
- "city/contry": "Chicago,IL  USA"
+ "city/country": "Chicago,IL  USA"
 },
 {	
  "team": "Las Vegas Raiders",
  "worth": 6.2,
  "Sport": "NFL Football", 
- "city/contry": "Las Vegas,NV  USA"
+ "city/country": "Las Vegas,NV  USA"
 },
 {	
  "team": "New York Knick",
  "worth": 6.1,
  "Sport": "NBA Basketball", 
- "city/contry": "New York,NY  USA"
+ "city/country": "New York,NY  USA"
 },
 {	
  "team": "New York Jets",
  "worth": 6.1,
  "Sport": "NFL Football", 
- "city/contry": "New York,NY  USA"
+ "city/country": "New York,NY  USA"
 },
 {	
  "team": "Real Madrid",
  "worth": 6.1,
  "Sport": "Soccar", 
- "city/contry": "Madrid, Spain"
+ "city/country": "Madrid, Spain"
 },
 {	
  "team": "Washington Commanders",
  "worth": 6.1,
  "Sport": "NFL Football", 
- "city/contry": "Washington,DC  USA"
+ "city/country": "Washington,DC  USA"
 },
 {	
  "team": "Manchester United",
  "worth": 6.0,
  "Sport": "Soccar", 
- "city/contry": "Old Trafford, Greater Manchester, England"
+ "city/country": "Old Trafford, Greater Manchester, England"
 },
 {	
  "team": "San Francisco 49ers",
  "worth": 6.0,
  "Sport": "NFL Football", 
- "city/contry": "San Francisco,CA USA"
+ "city/country": "San Francisco,CA USA"
 },
 {	
  "team": "Los Angeles Lakers",
  "worth": 5.9,
  "Sport": "NBA Basketball", 
- "city/contry": "Los angeles,CA USA"
+ "city/country": "Los angeles,CA USA"
 }
 ]
 
-'''
-- which sports team is worth?
-	- list of teams and worth [dictionary w/ key and values]
- 
-		- comparison line: team, worth, sport, city/country
-	- comparing the teams
-		- make A vs B, once get it right change B to A then B is a different team
-	- letting the user choose between
-	- function deciding right and wrong
-	- add a counter on how many you got right, then once lost end game'''
--- main.py
+++ main.py
@@ -1,5 +1,54 @@
 from gamedata import teams
+import random
+
+'''def team_select():
+    for x in teams:                             #loop thru teams list
+        for key,value in x.items():             #select keys and values from dict. selected
+        
+            
+            
+            
+            
+            
+            
+team_select()'''
 
 
+def comparison(): 
+    A, B = random.sample(teams, k=2)    #random picks 2 dictionaries from list
+    print(f" {A['team']}, {A['worth']} Billion, Sprot: {A['Sport']}, From {A['city/country']}. ")
+    print(f" {B['team']}, {B['worth']} Billion, Sprot: {B['Sport']}, From {B['city/country']}. ")
+    while True:
+        user_pick = input("Which Sports Team is worth more? A or B").lower()
+        if user_pick == 'a':
+            if A["worth"] > B["worth"]:
+                print("Correct! ")  
+            else: 
+                print(f"Wrong answer, {A['worth']} Billion and {B['worth']} Billion. ")
+                break
+            B = random.sample(teams, k=1)
+            print(f" {A['team']}, {A['worth']} Billion, Sprot: {A['Sport']}, From {A['city/country']}. ")
+            print(f" {B['team']}, {B['worth']} Billion, Sprot: {B['Sport']}, From {B['city/country']}. ")
+        elif user_pick == "b":
+            if A["worth"] < B["worth"]:
+                print("Correct! ")
+            A == B 
+            B == random.sample(teams, k=1)
+            print(f" {A['team']}, {A['worth']} Billion, Sprot: {A['Sport']}, From {A['city/country']}. ")
+            print(f" {B['team']}, {B['worth']} Billion, Sprot: {B['Sport']}, From {B['city/country']}. ")
+        else: 
+            print(f"Wrong answer, {A['worth']} Billion and {B['worth']} Billion. ")
+            break
+            
 
-print(teams)
+
+comparison()
+'''
+- which sports team is worth?
+	
+		- comparison line: team, worth, sport, city/country
+	- comparing the teams
+		- make A vs B, once get it right change B to A then B is a different team
+	- letting the user choose between
+	- function deciding right and wrong
+	- add a counter on how many you got right, then once lost end game'''
